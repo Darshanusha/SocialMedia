75a9719a9a7d03af196de5307fd384fe7cb80d02
Darshan Usha <darshanusha2@gmail.com>
2020-09-20 19:52:42 +0530

    adding scroll bar from react-custom-scrollbars

--- src/components/NewsFeed.js
+++ src/components/NewsFeed.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import AddImage from './AddImage';
 import DisplayPosts from './DisplayPosts';
-import PerfectScrollbar from 'react-perfect-scrollbar'
+import { Scrollbars } from 'react-custom-scrollbars';
 class NewsFeed extends Component {
     render() {
         return (
-            <PerfectScrollbar>
-                <div className="container mt-2" >
-                    <div className="row">
-                        <div className="col-md-2 left-col" >Left</div>
-                        <div className="col-md-7 border bg-light center-col" >
-                            <div className="mt-2" >
+
+            <div className="container mt-2" >
+                <div className="row">
+                    <div className="col-md-2 left-col" >Left</div>
+                    <div className="col-md-7" >
+                        <Scrollbars style={{ width: "100%", height: "2500%" }} >
+                            <div className="mt-1 border bg-light center-col" width = "90%"  >
                                 <AddImage />
                                 <center className="mt-2" > News Feed..</center>
                                 <DisplayPosts />
                             </div>
-                        </div>
-                        <div className="col-md-3 right-col" >Right</div>
+                        </Scrollbars>
                     </div>
+                    <div className="col-md-3 right-col" >Right</div>
                 </div>
-            </PerfectScrollbar>
+            </div>
         );
     }
 }
